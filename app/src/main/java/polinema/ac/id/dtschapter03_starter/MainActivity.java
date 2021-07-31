d3a6967d7fd174b9549fa5e829d0679c1e471c22
od3ng <noprianto@polinema.ac.id>
2021-08-01 06:26:27 +0700

    migreate to androidx

--- app/src/main/java/polinema/ac/id/dtschapter03_starter/MainActivity.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/MainActivity.java
@@ -1,6 +1,6 @@
 package polinema.ac.id.dtschapter03_starter;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
